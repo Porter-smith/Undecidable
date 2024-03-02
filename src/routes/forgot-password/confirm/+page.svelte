4a2643efbbce99ca26884cc0fb86a65e725e44e8
Porter <portersmith@ymail.com>
2024-03-02 14:06:34 -0700

    i think this should work

--- src/routes/forgot-password/confirm/+page.svelte
+++ src/routes/forgot-password/confirm/+page.svelte
@@ -1,11 +1,16 @@
 <script>
 	import { meta } from '@/lib/metaTags';
+	import ConfirmPasswordForm from '@/components/ConfirmPasswordForm.svelte';
+	import { page } from '$app/stores';
 	const title = meta.confirmPassword.title;
 	const description = meta.confirmPassword.description;
+	const oobCode = $page.url.searchParams.get('oobCode');
+	console.log(oobCode);
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
 </svelte:head>
-<p>hio</p>
+
+<ConfirmPasswordForm {oobCode} />
