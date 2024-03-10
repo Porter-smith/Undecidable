06eb879e8c5a51bfbf20bd7d7a257d4d9dad4aca
Porter-smith <portersmith@ymail.com>
2024-03-10 16:56:29 -0600

    testing out posthog analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,17 @@
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
- 
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
 injectSpeedInsights();
 inject({ mode: dev ? 'development' : 'production' });
+if (browser) {
+    posthog.init(
+      'phc_x3RuhYg4kAzDOEwuldkIdptsKM5eHvzaX7xbhC9MRQ8',
+      { api_host: "https://us.posthog.com" }
+    )
+  }
+  return
+}
