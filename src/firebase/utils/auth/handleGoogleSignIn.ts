9e69f4c27749bd62033021cb5b3845c21b58b8f8
Porter-smith <portersmith@ymail.com>
2024-03-10 17:37:39 -0600

    remove sign in to flutter as it is not working

--- src/firebase/utils/auth/handleGoogleSignIn.ts
+++ src/firebase/utils/auth/handleGoogleSignIn.ts
@@ -3,32 +3,21 @@ import { auth } from '@/firebase/client';
 import fetchCreateUser from '@/services/api/auth/fetchCreateUser';
 import handleSignin from './handleServerSignin';
 
-function isRunningInFlutterWebView() {
-  return window.isFlutterInAppWebView === true;
-}
-
 async function handleGoogleSignIn() {
-  try {
-    if (isRunningInFlutterWebView()) {
-      // If the code is running in Flutter WebView, send a message to Flutter to handle.
-      window.Flutter.postMessage('openExternalBrowser');
-      return; // Stop execution in the WebView after sending the message
+    try {
+        const provider = new GoogleAuthProvider();
+        const { user } = await signInWithPopup(auth, provider);
+        const idToken = await user.getIdToken();
+
+        const isNewUser = getAdditionalUserInfo(user)?.isNewUser;
+        if (isNewUser) {
+            await fetchCreateUser(idToken);
+        }
+
+        await handleSignin(idToken);
+    } catch (error) {
+        console.error('Error during Google Sign In or setting user role:', error);
     }
-
-    // Proceed with the normal sign-in process if not in a Flutter WebView
-    const provider = new GoogleAuthProvider();
-    const { user } = await signInWithPopup(auth, provider);
-    const idToken = await user.getIdToken();
-
-    const additionalUserInfo = getAdditionalUserInfo(user);
-    if (additionalUserInfo?.isNewUser) {
-      await fetchCreateUser(idToken);
-    }
-
-    await handleSignin(idToken);
-  } catch (error) {
-    console.error('Error during Google Sign In or setting user role:', error);
-  }
 }
 
 export default handleGoogleSignIn;
