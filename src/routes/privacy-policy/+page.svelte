128ecc6a19660c10dc0600b9ea2d63017ed96a08
Porter-smith <portersmith@ymail.com>
2024-03-09 21:44:35 -0700

    update privacy policy

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -12,14 +12,47 @@
 </svelte:head>
 
 <div
-	class="text-text-color flex w-full items-center justify-center bg-neutral-900 px-5 py-20 text-center text-base"
+	class="text-text-color b flex w-full items-center justify-center px-5 py-20 text-center text-base"
 >
 	<div class="flex flex-col items-center">
 		<img src={UndecidableLogo} alt="Undecidable Logo" class="mb-4 w-32" />
 		<h1 class="mb-4 text-2xl font-semibold leading-5">Privacy Policy</h1>
-		<p class="max-w-md text-sm leading-5 tracking-tight text-neutral-400">
-			Your Privacy Policy content goes here. This should detail how you collect, use, and manage
-			your users' data.
-		</p>
+		<div class="max-w-md text-sm leading-5 tracking-tight text-neutral-200">
+			<p>Effective date: March 9, 2024</p>
+			<p>
+				The developers of "Undecidable" operate the Undecidable mobile application and website. This
+				page informs you of our non-collection policy regarding personal data when you use our
+				Service.
+			</p>
+			<p><strong>Non-Collection of Personal Data:</strong></p>
+			<p>
+				We do not collect, store, or process any personal data linked to users of our app or
+				website. The search queries you enter are used solely to provide movie or TV show
+				recommendations and are not stored or associated with any user identity.
+			</p>
+			<p><strong>Log Data:</strong></p>
+			<p>
+				We collect only non-personally identifiable information in the event of an error to improve
+				our Service. This data is not linked to any personal identifiers.
+			</p>
+			<p><strong>No Third-Party Data Sharing:</strong></p>
+			<p>
+				Since we do not collect personal data, we do not share any data with third-party service
+				providers or partners.
+			</p>
+			<p><strong>Security:</strong></p>
+			<p>
+				While we do not collect personal data, we strive to protect the security of our Service.
+			</p>
+			<p><strong>Changes to This Privacy Policy:</strong></p>
+			<p>
+				Our Privacy Policy may change from time to time. We will post any policy changes on this
+				page.
+			</p>
+			<p>
+				If you have any questions about this privacy policy, please contact us at
+				contact@myundeciable.com
+			</p>
+		</div>
 	</div>
 </div>
