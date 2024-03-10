65e66e0dad56864c21019e8d7d0ecae043071091
Porter-smith <portersmith@ymail.com>
2024-03-10 17:02:01 -0600

    hide email and password fields in recordings

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,10 +8,15 @@ export const load = async () => {
 injectSpeedInsights();
 inject({ mode: dev ? 'development' : 'production' });
 if (browser) {
-    posthog.init(
-      'phc_x3RuhYg4kAzDOEwuldkIdptsKM5eHvzaX7xbhC9MRQ8',
-      { api_host: "https://us.posthog.com" }
-    )
+    posthog.init('phc_x3RuhYg4kAzDOEwuldkIdptsKM5eHvzaX7xbhC9MRQ8', {
+        session_recording: {
+            maskAllInputs: false,
+            maskInputOptions: {
+                password: true, // Highly recommended as a minimum!!
+                email: true
+        }
+    }
+    })
   }
   return
 }
