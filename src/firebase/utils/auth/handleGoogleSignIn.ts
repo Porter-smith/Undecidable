e2fb495aba106a566be62075715e4ac303cde494
Porter-smith <portersmith@ymail.com>
2024-03-11 16:28:34 -0600

    fix thing i broke on accident

--- src/firebase/utils/auth/handleGoogleSignIn.ts
+++ src/firebase/utils/auth/handleGoogleSignIn.ts
@@ -4,20 +4,20 @@ import fetchCreateUser from '@/services/api/auth/fetchCreateUser';
 import handleSignin from './handleServerSignin';
 
 async function handleGoogleSignIn() {
-    try {
-        const provider = new GoogleAuthProvider();
-        const { user } = await signInWithPopup(auth, provider);
-        const idToken = await user.getIdToken();
+	try {
+		const provider = new GoogleAuthProvider();
+		const userCredential = await signInWithPopup(auth, provider);
+		const additionalUserInfo = getAdditionalUserInfo(userCredential);
+		const idToken = await userCredential.user.getIdToken();
 
-        const isNewUser = getAdditionalUserInfo(user)?.isNewUser;
-        if (isNewUser) {
-            await fetchCreateUser(idToken);
-        }
+		if (additionalUserInfo && additionalUserInfo.isNewUser) {
+			await fetchCreateUser(idToken);
+		}
 
-        await handleSignin(idToken);
-    } catch (error) {
-        console.error('Error during Google Sign In or setting user role:', error);
-    }
+		await handleSignin(idToken);
+	} catch (error) {
+		console.error('Error during Google Sign In or setting user role:', error);
+	}
 }
 
 export default handleGoogleSignIn;
