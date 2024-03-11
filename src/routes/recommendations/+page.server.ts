c06f4b05fb31f1b7b37f68881a08e34490b649bc
Porter-smith <portersmith@ymail.com>
2024-03-11 16:29:05 -0600

    commit this out for now

--- src/routes/recommendations/+page.server.ts
+++ src/routes/recommendations/+page.server.ts
@@ -1,15 +1,15 @@
-import getRecommendationsForUser from '../../lib/server/firebase/recommendations/getRecommendationForUser';
-import type { PageServerLoad } from './$types';
+// import getRecommendationsForUser from '../../lib/server/firebase/recommendations/getRecommendationForUser';
+// import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	const user = locals.user;
-	let recomendations = [];
-	if (user) {
-		recomendations = await getRecommendationsForUser(user.uid);
-	}
-	console.log(user.uid);
+// export const load: PageServerLoad = async ({ locals }) => {
+// 	const user = locals.user;
+// 	let recomendations = [];
+// 	if (user) {
+// 		recomendations = await getRecommendationsForUser(user.uid);
+// 	}
+// 	console.log(user.uid);
 
-	return {
-		recomendations
-	};
-};
+// 	return {
+// 		recomendations
+// 	};
+// };
